a14b5f1a40026f1dc7f907125b35c3938a49afa3
Kevin Leimkuhler <kevleimkuhler@gmail.com>
2015-12-12 23:54:17 -0500

    Antigen plugin location changed to within repo

--- zsh/bundles.zsh
+++ zsh/bundles.zsh
@@ -3,3 +3,6 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Guess what to install when running an unknown command
 antigen bundle command-not-found
+
+# Add color to man pages
+antigen bundle colored-man-pages
