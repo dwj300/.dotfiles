aaf6a03111ed448a27fc3c2d297c9b275702f6e2
Douglas Jordan <dwj300@gmail.com>
2016-11-14 14:58:37 -0500

    java 1.8

--- zsh/env_vars.zsh
+++ zsh/env_vars.zsh
@@ -4,5 +4,5 @@ export PYTHONSTARTUP=~/.pythonrc
 # ToDo: always?Make sure this doesn't break on OS X
 export TERM=xterm-256color
 
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
